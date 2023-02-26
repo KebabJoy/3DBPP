7e972af4d1f64b7d98376f9147769e06be88a04e
Nikas <niks@level.travel>
2023-02-26 22:06:39 +0300

    finish..1st day

--- box.cpp
+++ box.cpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Box.hpp"
+#include "include/box.hpp"
 
 Box::Box(double weight, double length, double width, double height, double gravityCenterX, double gravityCenterY, double gravityCenterZ)
 : weight(weight), length(length), width(width), height(height), gravityCenterX(gravityCenterX), gravityCenterY(gravityCenterY), gravityCenterZ(gravityCenterZ) {}
@@ -59,4 +59,18 @@ void Box::setY(double y_) {
 
 void Box::setZ(double z_) {
     this->z = z_;
+}
+
+void swap(Box& lhs, Box& rhs) {
+    using std::swap;
+    swap(lhs.weight, rhs.weight);
+    swap(lhs.length, rhs.length);
+    swap(lhs.width, rhs.width);
+    swap(lhs.height, rhs.height);
+    swap(lhs.gravityCenterX, rhs.gravityCenterX);
+    swap(lhs.gravityCenterY, rhs.gravityCenterY);
+    swap(lhs.gravityCenterZ, rhs.gravityCenterZ);
+    swap(lhs.x, rhs.x);
+    swap(lhs.y, rhs.y);
+    swap(lhs.z, rhs.z);
 }
